845845b3173db86095909ae05a32ea4b76773467
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-12-22 17:17:14 +0100

    Use stdint

--- src/c/asm.h
+++ src/c/asm.h
@@ -3,6 +3,6 @@
 
 #include <stdint.h>
 
-extern void rgb2grayscale(unsigned char *data, int64_t width, int64_t height, int32_t *channels);
+extern void rgb2grayscale(uint8_t *data, int32_t width, int32_t height, int32_t *channels);
 
 #endif
