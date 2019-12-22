c7fb92618e81669d2bd71068e865d1736304166e
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-12-22 15:35:10 +0100

    Add imax and imin

--- src/asm/math.asm
+++ src/asm/math.asm
@@ -1,4 +1,6 @@
 global arctan2
+global imax
+global imin
 
 section .text
 
@@ -28,3 +30,38 @@ arctan2:
     pop     rbp
     ret
 
+; Compute max of two signed integers
+; Params:
+;     rdi: integer x
+;     rsi: integer y
+; Return:
+;     rax: max(x,y)
+imax:
+    push    rbp
+    mov     rbp, rsp
+
+    mov     rax, rdi
+    cmp     rax, rsi
+    cmovl   rax, rsi
+
+    mov     rsp, rbp
+    pop     rbp
+    ret
+
+; Compute min of two signed integers
+; Params:
+;     rdi: integer x
+;     rsi: integer y
+; Return:
+;     rax: min(x,y)
+imin:
+    push    rbp
+    mov     rbp, rsp
+
+    mov     rax, rdi
+    cmp     rsi, rax
+    cmovl   rax, rsi
+
+    mov     rsp, rbp
+    pop     rbp
+    ret
