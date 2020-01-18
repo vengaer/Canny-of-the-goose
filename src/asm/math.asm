e469c76a03f137f20ea291cb20872ef144ddfe35
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2020-01-18 16:01:56 +0100

    Add arctan for packed xmm register

--- src/asm/math.asm
+++ src/asm/math.asm
@@ -1,33 +1,86 @@
     section .text
     global arctan2
+    global arctan2pckd
     global lerp
     global imax
     global imin
 
 ; Compute arctan(y/x)
 ; Params:
-;     xmm0 (scalar double precision): y
-;     xmm1 (scalar double precision): x
+;     xmm0 (scalar single precision): y
+;     xmm1 (scalar single precision): x
 ; Return:
-;     xmm0 (scalar double precision): result of the computation
+;     xmm0 (scalar single precision): result of the computation
 arctan2:
-.y  equ     8
+.y  equ     4
 .x  equ     0
     sub     rsp, 16                         ; Local storage
 
-    movsd   qword [rsp + .y], xmm0
-    movsd   qword [rsp + .x], xmm1
+    movss   dword [rsp + .y], xmm0
+    movss   dword [rsp + .x], xmm1
 
-    fld     qword [rsp + .y]                ; Push to fp stack
+    fld     dword [rsp + .y]                ; Push to fp stack
     fst     st1                             ; Copy st0 to st1
-    fld     qword [rsp + .x]
+    fld     dword [rsp + .x]
 
     fpatan                                  ; atan(st1 / st0), pop
-    fstp    qword [rsp + .x]                ; Pop result to stack
-    movsd   xmm0, qword [rsp + .x]
+    fstp    dword [rsp + .x]                ; Pop result to stack
+    movss   xmm0, dword [rsp + .x]
 
     add     rsp, 16                         ; Restore rsp
     ret
+
+; Compute arctan(y/x) for packed registers for convenience
+; Params:
+;     xmm0 (packed single precision): ys
+;     xmm1 (packed single precision): xs
+; Return:
+;     xmm0 (packed single precision): results of the computations
+arctan2pckd:
+    pxor    xmm14, xmm14
+    movaps  xmm12, xmm0                     ; Preserve data
+    movaps  xmm13, xmm1
+
+    psrldq  xmm0, 12                        ; Compute atan of 4 most significant bytes
+    psrldq  xmm1, 12
+
+    call    arctan2
+
+    movss   xmm14, xmm0                     ; Store in xmm14 and shift out of the way
+    pslldq  xmm14, 4
+
+    movaps  xmm0, xmm12                     ; Restore
+    movaps  xmm1, xmm13
+
+    psrldq  xmm0, 8                         ; Next 4 bytes
+    psrldq  xmm1, 8
+
+    call    arctan2
+
+    movss   xmm14, xmm0                     ; Store in lower 4 bytes of xmm14
+    pslldq  xmm14, 4
+
+    movaps  xmm0, xmm12                     ; Restore
+    movaps  xmm1, xmm13
+
+    psrldq  xmm0, 4                         ; Next 4 bytes
+    psrldq  xmm1, 4
+
+    call    arctan2
+
+    movss   xmm14, xmm0                     ; Store in xmm14
+    pslldq  xmm14, 4
+
+    movaps  xmm0, xmm12                     ; Restore
+    movaps  xmm1, xmm13
+
+    call    arctan2
+
+    movss   xmm14, xmm0                     ; Store in xmm14
+    movaps  xmm0, xmm14                     ; Result to xmm0
+
+    ret
+    
 
 ; Linear interpolation of dwords
 ; Params:
