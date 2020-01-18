d88c3cd7bf90f28b1370748aea3ad6dd1346d993
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2020-01-18 14:59:42 +0100

    Add linear interpolation

--- src/asm/math.asm
+++ src/asm/math.asm
@@ -1,5 +1,6 @@
     section .text
     global arctan2
+    global lerp
     global imax
     global imin
 
@@ -26,6 +27,28 @@ arctan2:
     movsd   xmm0, qword [rsp + .x]
 
     add     rsp, 16                         ; Restore rsp
+    ret
+
+; Linear interpolation of dwords
+; Params:
+;     edi: value of lower pixel
+;     esi: value of upper pixel
+;     xmm0 (scalar single precision): fraction of distance between edi and esi
+; Return:
+;     eax: interpolated value
+lerp:
+    pxor    xmm1, xmm1
+    pxor    xmm2, xmm2
+
+    cvtsi2ss    xmm1, edi
+    cvtsi2ss    xmm2, esi
+
+    subss   xmm2, xmm1                      ; Difference between pixels
+    mulss   xmm2, xmm0                      ; Multiply by fraction
+    addss   xmm2, xmm1                      ; Add value of lower pixel
+
+    cvtss2si    eax, xmm2                   ; Result to eax
+
     ret
 
 ; Compute max of two signed integers
