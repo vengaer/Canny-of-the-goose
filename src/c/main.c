423e0ec08a113608498c5acfc7d03cb7fd39e3b6
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2020-02-01 17:23:55 +0100

    Allow passing of thresholds from command line

--- src/c/main.c
+++ src/c/main.c
@@ -4,12 +4,17 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <argp.h>
+#include <regex.h>
 #include <unistd.h>
 
 #define DEFAULT_OUTFILE "data/cotg_out.png"
+
+static regex_t float_rgx;
 
 char const *argp_program_version = "canny-of-the-goose 1.0";
 char const *argp_program_bug_address = "<vilhelm.engstrom@tuta.io>";
@@ -18,13 +23,16 @@ static char doc[] = "cotg (Canny of the Goose) -- Canny edge detection";
 static char args_doc[] = "";
 
 static struct argp_option options[] = {
-    {"infile",  'i', "IMAGE", 0, "Specify infile (required)", 0 },
-    {"outfile", 'o', "OUT",   0, "Specify file to save image to", 0 },
+    {"infile",  'i', "IMAGE",          0, "Specify infile (required)", 0 },
+    {"outfile", 'o', "OUT",            0, "Specify file to save image to", 0 },
+    {"high",    'h', "HIGH_THRESHOLD", 0, "High threshold", 0 },
+    {"low",     'l', "LOW_THRESHOLD",  0, "Low threshold", 0 },
     { 0 }
 };
 
 struct arguments {
     char const *infile, *outfile;
+    float high, low;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -36,10 +44,40 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
         case 'o':
             args->outfile = arg;
             break;
+        case 'h':
+            if(regexec(&float_rgx, arg, 0, NULL, 0)) {
+                fprintf(stderr, "%s is not a floating point\n", arg);
+                return ARGP_ERR_UNKNOWN;
+            }
+            args->high = atof(arg);
+            break;
+        case 'l':
+            if(regexec(&float_rgx, arg, 0, NULL, 0)) {
+                fprintf(stderr, "%s is not a floating point\n", arg);
+                return ARGP_ERR_UNKNOWN;
+            }
+            args->low = atof(arg);
+            break;
         default:
             return ARGP_ERR_UNKNOWN;
     }
     return 0;
+}
+
+static inline bool thresholds_valid(struct arguments const *args) {
+    if(args->low < 0.f) {
+        fputs("Low threshold must be positive\n", stderr);
+        return false;
+    }
+    if(args->high > 1.f) {
+        fputs("High threshold may not be larger than 1.0\n", stderr);
+        return false;
+    }
+    if(args->low >= args->high) {
+        fputs("Low threshold must be smaller than high threshold\n", stderr);
+        return false;
+    }
+    return true;
 }
 
 static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
@@ -47,8 +85,15 @@ static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 int main(int argc, char **argv) {
     struct arguments args = {
         .infile = NULL,
-        .outfile = DEFAULT_OUTFILE
+        .outfile = DEFAULT_OUTFILE,
+        .high = 0.5f,
+        .low = 0.05f
     };
+
+    if(regcomp(&float_rgx, "^[0-9]+(\\.[0-9]*f?)?$", REG_EXTENDED)) {
+        fputs("Failed to compile float regex\n", stderr);
+        return 1;
+    }
 
     argp_parse(&argp, argc, argv, 0, 0, &args);
     if(!args.infile) {
@@ -57,6 +102,9 @@ int main(int argc, char **argv) {
     }
     if(access(args.infile, F_OK)) {
         perror("Error accessing infile");
+        return 1;
+    }
+    if(!thresholds_valid(&args)) {
         return 1;
     }
     int width, height, channels;
@@ -94,7 +142,7 @@ int main(int argc, char **argv) {
     }
     printf("%-20s%d\n", "Edge detection:", edge_status);
 
-    dbl_threshold(texdata, width, height, 0.05f, 0.5f);
+    dbl_threshold(texdata, width, height, args.low, args.high);
     printf("%-20s%d\n", "Double threshold:", 0); /* Cannot fail */
 
     int hysteresis_status = hysteresis(texdata, width, height);
